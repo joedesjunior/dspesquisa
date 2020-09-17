7b02b224bbf819340f0d41a7169a204e5e4c4855
joedesjunior <joedes.freitas@gx2.com.br>
2020-09-17 11:49:39 -0300

    DTO, services

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Record.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Record.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_record")
 
@@ -28,6 +30,7 @@ public class Record implements Serializable {
 	private Integer age;
 	private Instant moment;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "game_id")
 	private Game game;
--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/GenreRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GenreRepository.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dspesquisa.entities.Genre;
 
 import com.devsuperior.dspesquisa.entities.Genre;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 	
 
